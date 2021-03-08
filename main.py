71295b1336893aeb9ffb6496c554bc0b90482c2f
repl.it user <replituser@example.com>
2021-03-08 20:46:43 +0000

    Added comparison operators programme

---
+++ main.py
@@ -0,0 +1,14 @@
+print(" Please enter the first number.")
+first_number=int(input())
+print("First number is", first_number)
+print(" Please enter the second number.")
+second_number=int(input())
+print("The second number is", second_number)
+if(first_number>second_number):
+  print( "First number \"",first_number,"\" is bigger than second number \"" ,second_number,"\".")
+elif(first_number<second_number):
+  print("First number \"",first_number,"\" is smaller than second number \"", second_number,"\".")
+else:
+  print("The two numbers are equal one to each other.")
+print("\n\t\t End of the game!")
+
